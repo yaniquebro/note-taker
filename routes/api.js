ba3cc318802fa4c233e8e872cabcf8866f780079
Yanique Brooks <yaniquebro@gmail.com>
2024-07-20 14:07:03 -0400

    api file

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,42 @@
-const router = express.Router();
+const router = require("express");
+const fs = require('fs');
+const uuid4 = uuid.v4()
+
+//export router
+module.exports = router;
+
+//GET notes
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(404).json({ message: "note not found"});
+        }
+
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+//POST note
+router.post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(404).json({ message: "note not found"});
+        }
+
+        const notes = JSON.parse(data);
+        console.log(req.body);
+        const newNote = {
+            ...req.body,
+            id: uuvidv4(),
+        };
+
+        notes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+            console.log(err);
+
+        });
+    });
+});
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 //import express
 const express = require('express');
 const path = require('path');
-
+const routes = require('./routes')
 //create instance of express
 const app = express();
+
+//set the port
+const PORT = process.env.PORT || 3001;
 
 //use app joins to puplic folder
 app.use(express.static('public'));
 
-//to route files
-app.use('/apiRoutes', require('./routes/apiRoutes'));
-
-//set the port
-const PORT = process.env.PORT || 3001;
+//middleware 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 // listen for port
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
