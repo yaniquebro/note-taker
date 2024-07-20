ba56ebc3b6cb657e654b9b3cef44aefe3fd5ac3f
Yanique Brooks <yaniquebro@gmail.com>
2024-07-20 13:10:55 -0400

    nodemon, routes folder

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+//import express
 const express = require('express')
 const path = require('path');
 
+//create instance of express
 const app = express();
 
+//use app joins to puplic folder
+app.use(express.static('public'));
+
+//set the port
 const PORT = process.env.PORT || 3001;
 
+// listen for port
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
